451144f6a62c6bad818e58299134a4e0097a492d
xomaxy <angelcellwall@gmail.com>
2024-01-18 23:21:53 -0600

    Added skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script>
     import "../app.css";
+ 
+    import { onMount } from "svelte";
+    import { autoModeWatcher } from '@skeletonlabs/skeleton';
+
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+
+    onMount(() => {										
+        autoModeWatcher();
+    })
 </script>
+
+<LightSwitch/>
   
 <slot />
 
