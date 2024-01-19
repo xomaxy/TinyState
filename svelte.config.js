2a77bae01beda2e9789263a39c60d15279012595
xomaxy <angelcellwall@gmail.com>
2024-01-18 22:56:46 -0600

    Added styles

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
-
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+			adapter: adapter({
+				fallback: '404.html'
+			}),
+			paths: {
+				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			},
+		},
+	preprocess: vitePreprocess()
 };
 
 console.log(JSON.stringify(config));
